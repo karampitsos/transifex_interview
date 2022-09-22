353480e4ea579ddf4d9cfd8377fe1497227c147a
karampitsos <nikolaos.karampitsos@gmail.com>
2022-09-22 11:47:12 +0300

    last

--- transifex/pipeline.py
+++ transifex/pipeline.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Awaitable, List
+from typing import Awaitable, List, Dict
 import asyncio
 from transifex.clients import Client
 from transifex.parsers import Parser
@@ -9,13 +9,13 @@ from transifex.transifex_client import TransifexClient
 class Pipeline:
 
     async def run(self, consumer: Client, parser: Parser,
-                  client: TransifexClient, resource: str):
+                  client: TransifexClient, resource: str) -> Dict:
         data = await consumer.get()
         parsed = parser.parse(data)
         response = await client.send(resource, parsed)
         return response
 
     @classmethod
-    async def run_pipelines(cls, pipelines: List[Awaitable]):
+    async def run_pipelines(cls, pipelines: List[Awaitable]) -> List[Dict]:
         results = await asyncio.gather(*pipelines)
         return results
